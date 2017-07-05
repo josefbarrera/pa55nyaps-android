0bb2fb528d52d038cdac77bdd1260f46913f7b68
José Barrera <contact@josebb.com>
2017-07-05 08:15:05 +0100

    Tidying up the GUI of the Start Screen
    1. Fix the title's text size and make it all uppercase
    2. Change the background color of the items in the password settings
    databases list
    3. Listing of password settings databases
    4. Change the layout of file listing to LinearLayout to accommodate delete button
    5. Logic to delete password settings databases, work in progress
    6. Remove unnecessary tools:ignore=InefficientWeight value

--- app/src/main/java/prototype/pa55nyaps/gui/StartScreen.java
+++ app/src/main/java/prototype/pa55nyaps/gui/StartScreen.java
@@ -1,18 +1,25 @@
 package prototype.pa55nyaps.gui;
 
+import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.TextView;
 
 import com.pa55nyaps.pa55nyaps.R;
 
 import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
 
 public class StartScreen extends AppCompatActivity
                          implements CreateDatabaseFragment.DialogListener {
@@ -51,6 +58,8 @@ public class StartScreen extends AppCompatActivity
         switch (item.getItemId()) {
             case R.id.addDatabase:
                 return true;
+            case R.id.edit_settings_databases:
+                editSettingsDatabases();
             default:
                 // If we get here, the user's action was not recognised.
                 // Invoke the superclass to handle it.
@@ -63,23 +72,55 @@ public class StartScreen extends AppCompatActivity
         renderFileListing();
     }
 
+    public void editSettingsDatabases() {
+        Log.i("Debugging", "Clicked on edit");
+    }
+
     private void renderFileListing() {
         String db_directory_name = getResources().getString(R.string.databases_directory);
         File databases_directory = new File(getBaseContext().getFilesDir(), db_directory_name);
         if (!databases_directory.isDirectory()) {
-            Log.i("Create directoy", "Creating parent directoy " + databases_directory.getName());
+            Log.i("Create directory", "Creating parent directory " + databases_directory.getName());
             databases_directory.mkdir();
         }
         files = databases_directory.listFiles();
+        TextView helpText = (TextView) findViewById(R.id.createDbHelpText);
+        ListView filesListing = (ListView) findViewById(R.id.filesListing);
 
-        Log.i("Stat debugging", "Start listing " + files.length + " files");
-        for (File file : files) {
-            Log.i("File found", file.getName());
+        Log.i("Number of files", "There are currently " + files.length + " files");
+        if (files.length == 0) {
+            // Make help text visible and files listing invisible
+            helpText.setVisibility(View.VISIBLE);
+            filesListing.setVisibility(View.GONE);
+            return;
         }
-        // TODO
-        // List all databases if any found
-        // otherwise show the help text
-        // call renderFileListing when a database is deleted
+
+        // Set the file listing footer
+        View footerView = getLayoutInflater().inflate(R.layout.file_listing_footer_layout, null, false);
+        filesListing.addFooterView(footerView);
+
+        // Hide the help text
+        helpText.setVisibility(View.GONE);
+
+        ArrayList<HashMap<String, String>> filesList = new ArrayList<>();
+        for (File file : files) {
+            Date unformattedLastModified = new Date(file.lastModified());
+            SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd hh:mm");
+            String lastModified= "Updated: " + formatter.format(unformattedLastModified);
+
+            HashMap<String, String> fileDetails = new HashMap<>();
+            fileDetails.put("fileName", file.getName());
+            fileDetails.put("lastModified", lastModified);
+            filesList.add(fileDetails);
+        }
+
+        String[] from = {"fileName", "lastModified"};
+        int[] to = {R.id.fileName, R.id.lastModified};
+
+        // Add the file items to the list view and display it
+        filesListing.setAdapter(new SimpleAdapter(StartScreen.this, filesList, R.layout.file_list_layout, from, to));
+        filesListing.setVisibility(View.VISIBLE);
+        // TODO call renderFileListing when a database is deleted
 
     }
 }
